e2e4ce7f40e5c1b31551dbbf1913c7821225cb2d
liuyi.hui <liuyi.hui@rea-group.com>
2021-07-14 22:55:42 +0800

    Add hello component

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from "./Hello";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Hello name="TypeScript" enthusiasmLevel={10} />,
+    document.getElementById('root') as HTMLElement
 );
 
 // If you want to start measuring performance in your app, pass a function
